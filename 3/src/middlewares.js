35d96a6b725361ad389df70e32f153a2b6e62ad5
rrraumpanzer <ayylmao3713@gmail.com>
2024-10-25 22:56:27 +0300

    Озарение

--- 3/src/middlewares.js
+++ 3/src/middlewares.js
@@ -11,16 +11,7 @@ const addDate = (store) => (next) => (action) => {
   // BEGIN (write your solution here)
   if (action.type === 'TASK_ADD') {
     const currentDate = new Date().toLocaleDateString('ru-RU');
-    const addText = {
-      ...action,
-      payload: {
-        task: {
-          ...action.payload.task,
-          text: `Задача на ${currentDate}: ${action.payload.task.text}`
-        }
-      }
-    };
-    return next(addText);
+    action.payload.task.text = 'Задача на ' + currentDate + ': ' + action.payload.task.text
   }
   return next(action);
   // END
